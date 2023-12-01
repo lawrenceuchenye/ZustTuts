c84987d9c3082c5b3901981633d2b2e2c407aa1c
lawrenceuchenye <lawuche29@gmail.com>
2023-12-01 18:39:45 +0000

    boiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import Column from "./components/Column";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <h1>Hellobworld!</h1>
+      <Column state="PLANNED" />
+      <Column state="ONGOING" />
+      <Column state="DONE" />
     </div>
   );
 }
---
+++ src/components/Column.jsx
@@ -0,0 +1,7 @@
+import "./Column.css";
+
+const Column = ({ state }) => {
+  return <div className="column">{state}</div>;
+};
+
+export default Column;
